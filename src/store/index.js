9f977cb8c78a5dcbe3615aa86e52bd2a51ab51cb
Sblaise123 <bsernaggio@gmail.com>
2023-12-06 19:37:43 -0500

    AYM website

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+import "../styles/index.css";
+
+
+import Layout from "./layout";
+
+
+ReactDOM.render(<Layout />, document.querySelector("#app"));
